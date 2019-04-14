adb1e0f089578ebc996d1b7349c6ac2f90ac7ce0
jeffshee <jeffshee8969@gmail.com>
2019-04-15 04:19:54 +0900

    Give up showing dialog on the IM, use a better approach

--- app/src/main/java/io/github/jeffshee/linestickerkeyboard/View/StickerKeyboardView.java
+++ app/src/main/java/io/github/jeffshee/linestickerkeyboard/View/StickerKeyboardView.java
@@ -1,13 +1,16 @@
 
 package io.github.jeffshee.linestickerkeyboard.View;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
 import android.content.Context;
+import android.graphics.Color;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -26,42 +29,43 @@ import io.github.jeffshee.linestickerkeyboard.R;
 import io.github.jeffshee.linestickerkeyboard.Util.FileHelper;
 import io.github.jeffshee.linestickerkeyboard.Util.SharedPrefHelper;
 
-import static android.content.Context.INPUT_METHOD_SERVICE;
-
 /* Summary
     IMService <-> StickerKeyboardView -- [ HistoryPackView <- HistoryAdapter <- HistoryPack
              StickerViewPagerAdapter -^  [ StickerPackView <- StickerAdapter <- StickerPack
                                          [      ``                  ``              ``
  */
-public class StickerKeyboardView extends LinearLayout {
+public class StickerKeyboardView extends LinearLayout implements View.OnClickListener {
     ArrayList<View> views = new ArrayList<>();
     HistoryPackView historyPackView;
     private HistoryPack historyPack;
+    private Context imService;
+    private View panel;
 
     public StickerKeyboardView(Context context) {
         super(context);
-        init(context);
+        this.imService = context;
+        init();
     }
 
-    private void init(final Context context) {
+    private void init() {
         setOrientation(LinearLayout.HORIZONTAL);
         setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
 
         // https://stackoverflow.com/questions/36924481/you-need-to-use-a-theme-appcompat-theme-or-descendant-with-the-design-library
-        ContextThemeWrapper ctx = new ContextThemeWrapper(context, R.style.MyTheme);
+        ContextThemeWrapper ctx = new ContextThemeWrapper(imService, R.style.MyTheme);
         LayoutInflater.from(ctx).inflate(R.layout.keyboard_layout, this, true);
 
         // History Pack
-        historyPack = SharedPrefHelper.getHistoryFromPref(context);
-        historyPackView = new HistoryPackView(context, historyPack);
+        historyPack = SharedPrefHelper.getHistoryFromPref(imService);
+        historyPackView = new HistoryPackView(imService, historyPack);
         views.add(historyPackView);
 
         // Sticker Pack
         ArrayList<StickerPack> stickerPacksFiltered = new ArrayList<>();
-        for (StickerPack stickerPack : SharedPrefHelper.getStickerPacksFromPref(context)) {
+        for (StickerPack stickerPack : SharedPrefHelper.getStickerPacksFromPref(imService)) {
             if (stickerPack.getVisible()) {
                 stickerPacksFiltered.add(stickerPack);
-                views.add(new StickerPackView(context, stickerPack));
+                views.add(new StickerPackView(imService, stickerPack));
             }
         }
 
@@ -74,7 +78,7 @@ public class StickerKeyboardView extends LinearLayout {
         tabLayout.setupWithViewPager(viewPager);
         tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
 
-        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        LayoutInflater inflater = (LayoutInflater) imService.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         for (int i = 0; i < tabLayout.getTabCount(); i++) {
             View view;
             if (inflater != null) {
@@ -83,7 +87,7 @@ public class StickerKeyboardView extends LinearLayout {
                 if (i == 0)
                     icon.setImageDrawable(getResources().getDrawable(R.drawable.baseline_history_white_36));
                 else {
-                    File file = FileHelper.getPngFile(context, stickerPacksFiltered.get(i - 1).getFirstId());
+                    File file = FileHelper.getPngFile(imService, stickerPacksFiltered.get(i - 1).getFirstId());
                     Glide.with(this).load(file).into(icon);
                 }
                 TabLayout.Tab tab = tabLayout.getTabAt(i);
@@ -100,24 +104,23 @@ public class StickerKeyboardView extends LinearLayout {
 
         // Setting Button
         ImageButton button = findViewById(R.id.imageButton);
-        button.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (context instanceof IMService) {
-                    ((IMService) context).showSettingDialog();
-                }
-            }
-        });
+        button.setOnClickListener(this);
         button.setOnLongClickListener(new OnLongClickListener() {
             @Override
             public boolean onLongClick(View view) {
-                InputMethodManager im = (InputMethodManager) context.getSystemService(INPUT_METHOD_SERVICE);
-                if (im != null) {
-                    im.showInputMethodPicker();
-                }
+                if (imService instanceof IMService) ((IMService) imService).showIMPicker();
                 return true;
             }
         });
+
+        // Setting Panel and buttons
+        panel = findViewById(R.id.panel);
+        panel.setVisibility(INVISIBLE);
+        Button btnMenu = findViewById(R.id.btnMenu);
+        Button btnSwitch = findViewById(R.id.btnSwitch);
+        panel.setOnClickListener(this);
+        btnMenu.setOnClickListener(this);
+        btnSwitch.setOnClickListener(this);
     }
 
     public void refreshHistoryAdapter(Sticker sticker) {
@@ -125,5 +128,34 @@ public class StickerKeyboardView extends LinearLayout {
         historyPackView.adapter.update(historyPack);
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.panel:
+                panel.setVisibility(INVISIBLE);
+                break;
+
+            case R.id.imageButton:
+                panel.setVisibility(VISIBLE);
+                // Fancy background fading ;)
+                ObjectAnimator colorFade = ObjectAnimator.ofObject(panel, "backgroundColor",
+                        new ArgbEvaluator(),
+                        Color.argb(100, 0, 0, 0),
+                        Color.argb(200, 0, 0, 0));
+                colorFade.setDuration(300);
+                colorFade.start();
+                break;
+
+            case R.id.btnMenu:
+                if (imService instanceof IMService) ((IMService) imService).launchMainMenu();
+                panel.setVisibility(INVISIBLE);
+                break;
+
+            case R.id.btnSwitch:
+                if (imService instanceof IMService) ((IMService) imService).showIMPicker();
+                panel.setVisibility(INVISIBLE);
+                break;
+        }
+    }
 }
 
